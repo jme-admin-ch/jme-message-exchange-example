5577916d5affdf350afa8ce9d4e45ffa49b9f1b4
Fabien Cerf <fabien.cerf@bit.admin.ch>
2026-07-21 11:40:54 +0200

    JEAP-6505: Fixed PactProviderTest configuration

--- jme-message-exchange-service/src/test/java/ch/admin/bit/jeap/jme/messageexchangeservice/PactProviderTest.java
+++ jme-message-exchange-service/src/test/java/ch/admin/bit/jeap/jme/messageexchangeservice/PactProviderTest.java
@@ -1,15 +1,27 @@
 package ch.admin.bit.jeap.jme.messageexchangeservice;
 
 import ch.admin.bit.jeap.messageexchange.test.Pacticipants;
+import ch.admin.bit.jeap.messageexchange.web.MessageExchangeApplication;
 import ch.admin.bit.jeap.messageexchange.web.rest.model.PactProviderTestBase;
 import ch.admin.bit.jeap.messaging.mockkafka.KafkaMockTestConfig;
+import ch.admin.bit.jeap.security.test.jws.JwsBuilder;
 import ch.admin.bit.jeap.security.test.resource.configuration.JeapOAuth2IntegrationTestResourceConfiguration;
 import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.transaction.support.TransactionTemplate;
 
+@SpringBootTest(classes = MessageExchangeApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
+        properties = {
+                "logging.level.au.com.dius.pact=DEBUG",
+                "jeap.security.oauth2.resourceserver.authorization-server.issuer=" + JwsBuilder.DEFAULT_ISSUER,
+                "jeap.security.oauth2.resourceserver.authorization-server.jwk-set-uri=http://localhost:1235/message-exchange/.well-known/jwks.json",
+                "spring.cloud.aws.sqs.enabled=false"
+        }
+)
 @Import({
         JeapOAuth2IntegrationTestResourceConfiguration.class,
         KafkaMockTestConfig.class})
